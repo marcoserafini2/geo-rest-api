7ada0b4ff4d6251225fd7f0ee78a4a1d28edfd46
Marco Serafini <marcoserafini2@gmail.com>
2018-06-04 19:07:07 +0200

    Spring Initializr
    Spring Initializr - Bootstrap application

---
+++ src/main/java/it/ms/geoapi/GeoapiApplication.java
@@ -0,0 +1,12 @@
+package it.ms.geoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoapiApplication.class, args);
+	}
+}
